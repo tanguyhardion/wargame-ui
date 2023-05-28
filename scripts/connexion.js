645af54a590888bddfed641bb28ccc04b8dea6e3
tanguyhardion <45904983+tanguyhardion@users.noreply.github.com>
2023-05-28 18:36:49 +0200

    Numéros, placeholders

--- scripts/connexion.js
+++ scripts/connexion.js
@@ -1,13 +1,11 @@
-new Vivus('logo', { duration: 100 });
-
 const dialog = connexion.html.getElementById('dialog');
 const invite = connexion.html.getElementById('invite');
 const annuler = connexion.html.getElementById('annuler');
 
-invite.addEventListener('click', function(){
+invite.addEventListener('click', function () {
     dialog.setAttribute('open', true);
 })
 
-annuler.addEventListener('click', function(){
+annuler.addEventListener('click', function () {
     dialog.removeAttribute('open')
 });
