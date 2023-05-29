6d4ae24d01ebb323dac677be39da2ce18630ce29
tanguyhardion <45904983+tanguyhardion@users.noreply.github.com>
2023-05-29 17:19:57 +0200

    Background fog

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,16 @@
+VANTA.FOG({
+    el: "#fog",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    highlightColor: 0xc2c2c2,
+    midtoneColor: 0xc2c2c2,
+    lowlightColor: 0xc2c2c2,
+    baseColor: 0x0,
+    speed: 1.4
+})
+
+
 new Vivus('logo', { duration: 100 });
