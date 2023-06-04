59f92e2c6dba72549fcc41765208d6cda16276f1
tanguyhardion <45904983+tanguyhardion@users.noreply.github.com>
2023-06-04 19:15:57 +0200

    friends / intro / alert

--- scripts/PagePrincipale.js
+++ scripts/PagePrincipale.js
@@ -1,1 +1,22 @@
-const scroll = new LocomotiveScroll();
+Swal.fire({
+    title: 'Accueil',
+    text: 'Bienvenue dans le jeu ! Souhaites-tu avoir une visite de la page d\'accueil?',
+    icon: 'info',
+    showCancelButton: true,
+    confirmButtonText: 'Oui',
+    cancelButtonText: 'Non',
+    confirmButtonColor: '#4e6450',
+    cancelButtonColor: '#808080'
+}).then((result) => {
+    if (result.isConfirmed) {
+        introJs().start();
+    }
+});
+
+$('#start-game').click((e) => {
+    e.preventDefault();
+    const vivus = new Vivus('logo', { duration: 100 });
+    vivus.finish().play(-1, () => {
+        window.location.href = 'PagePrincipale.html';
+    });
+});
