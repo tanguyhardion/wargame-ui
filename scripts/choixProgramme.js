180f7cd5b34dad646c03d5be1da0a3a11f294277
tanguyhardion <45904983+tanguyhardion@users.noreply.github.com>
2023-06-08 23:23:55 +0200

    carousel

---
+++ scripts/choixProgramme.js
@@ -0,0 +1,10 @@
+new Splide('.splide', {
+    type: 'loop',
+    width: '90%',
+    padding: '50px',
+    wheel: true,
+    perPage: 5,
+    focus: 'center',
+    isNavigation: true,
+    pagination: false,
+}).mount(); 
