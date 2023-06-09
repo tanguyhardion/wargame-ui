40b137e868f04761b4414d29dc4384e62f966ba6
veaucaro <caroline.veau@utt.fr>
2023-06-09 23:14:20 +0200

    pop-up paramètres

---
+++ scripts/parametre.js
@@ -0,0 +1,54 @@
+  document.getElementById('audio').addEventListener('click', function() {
+    Swal.fire({
+      title: 'Attention',
+      text: 'Quitter sans sauvegarder les modifications ?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Ok',
+      cancelButtonText: 'Annuler'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        window.location.href = 'parametreAudio.html';
+      }
+    });
+  });
+  
+  document.getElementById('accessibilite').addEventListener('click', function() {
+    Swal.fire({
+      title: 'Attention',
+      text: 'Quitter sans sauvegarder les modifications ?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Ok',
+      cancelButtonText: 'Annuler'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        window.location.href = 'parametreAccessibilite.html';
+      }
+    });
+  });
+  
+  document.getElementById('langue').addEventListener('click', function() {
+    Swal.fire({
+      title: 'Attention',
+      text: 'Quitter sans sauvegarder les modifications ?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Ok',
+      cancelButtonText: 'Annuler'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        window.location.href = 'parametresLangues.html';
+      }
+    });
+  });
+  
+  document.getElementById('appliquer').addEventListener('click', function() {
+    Swal.fire({
+      title: 'Confirmation',
+      text: 'Modifications appliquées',
+      icon: 'success',
+      confirmButtonText: 'Ok'
+    });
+  });
+  
