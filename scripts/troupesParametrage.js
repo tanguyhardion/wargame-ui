d50438863c56cb9cfe0aa26a5baa198ecc2968f9
tanguyhardion <45904983+tanguyhardion@users.noreply.github.com>
2023-06-09 21:38:41 +0200

    Style

--- scripts/troupesParametrage.js
+++ scripts/troupesParametrage.js
@@ -1,9 +1,9 @@
 for (var i = 0; i < 15; i++) {
-    $('.soldats-container').append('<img src="./res/images/soldats/casque_soldat.png" alt="soldat_logo">');
+    $('.soldats-container').append('<img src="./res/images/soldats/casque_soldat.png" alt="helmet_logo">');
 }
 
 for (var i = 0; i < 4; i++) {
-    $('.soldats-container').append('<img src="./res/images/soldats/casque_elite.png" alt="soldat_logo">');
+    $('.soldats-container').append('<img src="./res/images/soldats/casque_elite.png" alt="helmet_logo">');
 }
 
-$('.soldats-container').append('<img src="./res/images/soldats/casque_general.png" alt="soldat_logo">');
+$('.soldats-container').append('<img src="./res/images/soldats/casque_general.png" alt="helmet_logo">');
