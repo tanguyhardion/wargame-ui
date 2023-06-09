64f12b9551c9a8b1d46fe6738509ac4984ff576e
veaucaro <caroline.veau@utt.fr>
2023-06-09 22:08:42 +0200

    pop-up Connexion

--- scripts/connexion.js
+++ scripts/connexion.js
@@ -1,7 +1,32 @@
-vex.dialog.prompt({
-    message: 'Veuillez choisir un pseudo :',
-    placeholder: 'Pseudo',
-    callback: function (value) {
-        console.log(value)
-    }
-})
+  document.getElementById('invite').addEventListener('click', function() {
+    Swal.fire({
+      title: 'Continuer en tant qu\'invité',
+      text: 'Veuillez choisir un pseudo :',
+      input: 'text',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonText: 'Ok',
+      cancelButtonText: 'Annuler'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const pseudo = result.value;
+        Swal.fire(
+          'Pseudo enregistré !',
+          'Votre pseudo est : ' + pseudo,
+          'success'
+        ).then(() => {
+          window.location.href = 'PagePrincipale.html'; // Redirection vers une autre page HTML
+        });
+      }
+    });
+  });
+
+  document.getElementById('connexion').addEventListener('click', function() {
+    Swal.fire({
+      title: 'Mot de passe ou login incorrect',
+      text: 'Veuillez réessayer.',
+      icon: 'error',
+      confirmButtonText: 'Ok'
+    });
+  });
+
