374e5f5454a23f2b27418c5cb5c6380e69d43234
tanguyhardion <45904983+tanguyhardion@users.noreply.github.com>
2023-06-10 15:55:47 +0200

    swiping

--- scripts/troupesParametrage.js
+++ scripts/troupesParametrage.js
@@ -6,7 +6,7 @@ for (var i = 0; i < 4; i++) {
     $('.soldats-container').append('<img src="./res/images/soldats/casque_elite.png" alt="helmet_logo">');
 }
 
-$('.soldats-container').append('<img src="./res/images/soldats/casque_general.png" alt="helmet_logo">');
+$('.soldats-container').append('<img src="./res/images/soldats/casque_maitre.png" alt="helmet_logo">');
 
 $('.soldats-container img')[0].classList.add('selected');
 
@@ -16,14 +16,59 @@ $('.soldats-container img').on('click', (element) => {
         target.toggleClass('selected');
         target.siblings().removeClass('selected');
     }
+    const src = target.attr('src');
+    if (src.includes('casque_soldat')) {
+        $('.soldat img').attr('src', './res/images/soldats/soldat.png');
+        $('.soldat .name').text('Soldat');
+        $('.health-bar span').text('30 PV');
+        const attributes = $('.attributes input');
+        attributes.each((index, element) => {
+            $(element).val(0);
+        });
+    } else if (src.includes('casque_elite')) {
+        $('.soldat img').attr('src', './res/images/soldats/soldat_elite.png');
+        $('.soldat .name').text('Soldat d\'élite');
+        $('.health-bar span').text('35 PV');
+        const attributes = $('.attributes input');
+        attributes.each((index, element) => {
+            if ($(element).attr('name') === 'constitution') {
+                $(element).val(5);
+            } else {
+                $(element).val(1);
+            }
+        });
+    }
+    else {
+        $('.soldat img').attr('src', './res/images/soldats/maitre_de_guerre.png');
+        $('.soldat .name').text('Maître de guerre');
+        $('.health-bar span').text('40 PV');
+        const attributes = $('.attributes input');
+        attributes.each((index, element) => {
+            if ($(element).attr('name') === 'constitution') {
+                $(element).val(10);
+            } else {
+                $(element).val(2);
+            }
+        });
+    }
 });
 
+var current = 0;
 
 $('.arrow-container').on('click', (element) => {
     let side = $(element.target).attr('class').split(' ')[1];
     let image = $('.soldat img');
     const sign = side === 'left' ? '-' : '';
     const inverseSign = side === 'left' ? '' : '-';
+    current = side === 'left' ? current - 1 : current + 1;
+    if (current >= 0) {
+        $('.soldats-container img')[current].classList.add('selected');
+        $('.soldats-container img').each((index, element) => {
+            if (index !== current) {
+                element.classList.remove('selected');
+            }
+        });
+    }
     image.css('transform', `translateX(${inverseSign}300px)`);
     image.css('filter', 'brightness(0)');
     setTimeout(() => {
@@ -43,7 +88,7 @@ $('.next').on('click', (element) => {
     let attribute = $(element.target).parent().attr('class').split(' ')[1];
     let input = $(`input[name=${attribute}]`);
     let value = parseInt(input.val());
-    if (value < 10) {
+    if (value < $(element.target).siblings('div').find(`input[name="${attribute}"]`).attr('max')) {
         input.val(value + 1);
         $('.points-left').text(parseInt($('.points-left').text()) - 1);
     }
