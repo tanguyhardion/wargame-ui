908327403f6a95e02c709aec2d5dfd6b1c77cd9f
veaucaro <caroline.veau@utt.fr>
2023-06-12 13:41:36 +0200

    inscription EN

--- EN/scripts/inscriptionEN.js
+++ EN/scripts/inscriptionEN.js
@@ -1,16 +1,16 @@
 document.getElementById('invite').addEventListener('click', function () {
-    showUsernamePrompt('Veuillez choisir un pseudo :', 'question');
+    showUsernamePrompt('Please choose a valid login :', 'question');
 });
 
 function showUsernamePrompt(message, icon) {
     Swal.fire({
-        title: 'Continuer en tant qu\'invité',
+        title: 'Continue as a guest',
         text: message,
         input: 'text',
         icon: icon,
         showCancelButton: true,
         confirmButtonText: 'OK',
-        cancelButtonText: 'Annuler',
+        cancelButtonText: 'Cancel',
         confirmButtonColor: '#4e6450',
         cancelButtonColor: '#808080'
     }).then((result) => {
@@ -19,7 +19,7 @@ function showUsernamePrompt(message, icon) {
             if (isValidUsername(pseudo)) {
                 showUsernameConfirmation(pseudo);
             } else {
-                showUsernamePrompt('Veuillez choisir un pseudo valide.', 'error');
+                showUsernamePrompt('Please choose a valid login.', 'error');
             }
         }
     });
@@ -31,13 +31,13 @@ function isValidUsername(username) {
 
 function showUsernameConfirmation(username) {
     Swal.fire({
-        title: 'Pseudo enregistré !',
-        text: 'Votre pseudo est : ' + username,
+        title: 'Login registered !',
+        text: 'Your login is : ' + username,
         icon: 'success',
         confirmButtonColor: '#4e6450',
         cancelButtonColor: '#808080'
     }).then(() => {
-        window.location.href = 'chargement.html';
+        window.location.href = 'chargementEN.html';
     });
 }
 
@@ -45,38 +45,38 @@ $('.inscription').on('click', (e) => {
     e.preventDefault();
     if ($('.password input').val() != $('.password-repeat input').val()) {
         Swal.fire({
-            title: 'Les mots de passe ne correspondent pas',
-            text: 'Veuillez réessayer.',
+            title: 'Passwords do not match',
+            text: 'Please try again.',
             icon: 'error',
             confirmButtonColor: '#4e6450',
             cancelButtonColor: '#808080'
         });
     } else if ($('.username input').val().length === 0) {
         Swal.fire({
-            title: 'Le pseudo ne peut pas être vide',
-            text: 'Veuillez réessayer.',
+            title: 'Login cannot be empty',
+            text: 'Please try again.',
             icon: 'error',
             confirmButtonColor: '#4e6450',
             cancelButtonColor: '#808080'
         });
     } else if ($('.password input').val().length === 0 || $('.password-repeat input').val().length === 0) {
         Swal.fire({
-            title: 'Le mot de passe ne peut pas être vide',
-            text: 'Veuillez réessayer.',
+            title: 'Password cannot be blank',
+            text: 'Please try again.',
             icon: 'error',
             confirmButtonColor: '#4e6450',
             cancelButtonColor: '#808080'
         });
     } else {
         Swal.fire({
-            title: 'Inscription réussie',
-            text: 'Vous allez être redirigé vers le jeu.',
+            title: 'Successful registration',
+            text: 'You will be redirected to the game.',
             icon: 'success',
             confirmButtonColor: '#4e6450',
             cancelButtonColor: '#808080'
         });
         setTimeout(() => {
-            window.location.href = 'chargement.html';
+            window.location.href = 'chargementEN.html';
         }, 2000);
     }
 });
