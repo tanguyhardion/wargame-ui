502b84d3d3c974ef92812bf67e011ab89f7636f8
tanguyhardion <45904983+tanguyhardion@users.noreply.github.com>
2023-06-12 21:43:20 +0200

    typos

--- EN/scripts/parametres.js
+++ EN/scripts/parametres.js
@@ -1,10 +1,10 @@
 document.getElementById('audio').addEventListener('click', function () {
     Swal.fire({
         title: 'Warning',
-        text: 'Exit without saving the changes?',
+        text: 'Exit without saving changes ?',
         icon: 'warning',
         showCancelButton: true,
-        confirmButtonText: 'Ok',
+        confirmButtonText: 'OK',
         cancelButtonText: 'Cancel',
         confirmButtonColor: '#4e6450',
         cancelButtonColor: '#808080'
@@ -18,10 +18,10 @@ document.getElementById('audio').addEventListener('click', function () {
 document.getElementById('accessibilite').addEventListener('click', function () {
     Swal.fire({
         title: 'Warning',
-        text: 'Exit without saving the changes?',
+        text: 'Exit without saving changes ?',
         icon: 'warning',
         showCancelButton: true,
-        confirmButtonText: 'Ok',
+        confirmButtonText: 'OK',
         cancelButtonText: 'Cancel',
         confirmButtonColor: '#4e6450',
         cancelButtonColor: '#808080'
@@ -35,10 +35,10 @@ document.getElementById('accessibilite').addEventListener('click', function () {
 document.getElementById('langue').addEventListener('click', function () {
     Swal.fire({
         title: 'Warning',
-        text: 'Exit without saving the changes?',
+        text: 'Exit without saving changes ?',
         icon: 'warning',
         showCancelButton: true,
-        confirmButtonText: 'Ok',
+        confirmButtonText: 'OK',
         cancelButtonText: 'Cancel',
         confirmButtonColor: '#4e6450',
         cancelButtonColor: '#808080'
@@ -51,8 +51,8 @@ document.getElementById('langue').addEventListener('click', function () {
 
 document.getElementById('appliquer').addEventListener('click', function () {
     Swal.fire({
-        title: 'Confirmation',
-        text: 'Changes applied',
+        title: 'Chanegs applied',
+        text: 'You can now safely exit the settings.',
         icon: 'success',
         confirmButtonText: 'Ok',
         confirmButtonColor: '#4e6450',
